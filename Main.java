838e7f69d7d850d96b017dbe6d7850e8005b84b5
leefitzgeraldretiro7-netizen <leefitzgeraldretiro7@gmail.com>
2025-08-23 13:21:34 +0000

    fitz

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[]args) {
 
+        System.out.println("SEATWORKFIVE");
+
+        int countdown = 3;
+
+        while (countdown > 0) {
+
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy New Year!!");
+    }
+}
