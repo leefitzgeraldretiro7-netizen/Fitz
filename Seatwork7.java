a678af237c52f280ef6f214fbd0632ea59e60d15
leefitzgeraldretiro7-netizen <leefitzgeraldretiro7@gmail.com>
2025-08-28 09:05:27 +0000

    Seatwork7&8

--- Seatwork7.java
+++ Seatwork7.java
@@ -1,1 +1,28 @@
+	public class Seatwork7 {
+	public static void main(String[] args) {
+		char grade = 'C';
+		
+		switch(grade){
+		    case 'A':
+		    System.out.println("Excellent");
+		    break;
+		    
+		    case 'B':
+		    case 'C':
+		    System.out.println("Welldone");
+		    
+		    break;
+		    case 'D':
+		    System.out.println("You passed");
+		    
+		    case 'F':
+		    System.out.println("Better try again");
+default:
+System.out.println("invalid grade");
+
+		}	    
+System.out.println("Your grade is" +  grade);
+		}
+
+	}
 
